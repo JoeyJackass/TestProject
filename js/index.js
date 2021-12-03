edbb13de47001ea2f29cb036d6d7061e9d4e9681
Poul <joeyjackasd@gmail.com>
2021-12-03 12:48:39 +0200

    first commi

---
+++ js/index.js
@@ -0,0 +1,87 @@
+// let num = 10;
+// while (num < 15){
+//     console.log(num);
+//     num++;
+// }
+
+// let i;
+// for (i = 5; i < 30; i++){
+//     console.log(i);
+// }
+
+// let sum = +prompt('Age', '');
+// if (sum ==  18){
+//     alert('привет');
+// }
+// else if (sum == 80){
+//     alert("WoW");
+// }
+// else if (sum == 16){
+//     alert('go away faggot');
+// }
+// else {alert('error');
+// }
+
+// let cum = +prompt('Name', '') ? ('User') : alert('Hello User') ? ('Boss') : alert('Gutten Tad Mister');
+// let planet = ('saturnus');
+// console.log(`my favorite place in univercity is ${planet}`);
+
+// let enter = prompt('кто там?', '');
+// if (enter == null){
+//     alert('Отменено!');
+// }
+// if (enter == '' || null){
+//     alert('Я вас не знаю!');
+// }
+// if (enter == 'админ'){
+//     let pass = prompt('Пароль', '');
+//     if (pass == null){
+//         alert('Отменено!');
+//     }
+//     if (pass == '' || null){
+//         alert('Я вас не Знаю!');
+//     }
+//     if (pass == 'я главный'){
+//         alert('Здравствуйте!');
+//     }
+// }
+
+
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+
+
+// function plus(num1, num2){
+//     console.log(num1 + num2);
+// }
+// plus(6,4);
+
+
+// function checkAge(age){
+//     if (age > 18){
+//         return true;
+//     }
+//     else{
+//         alert("а родители разрешили?");
+//     }
+// }
+
+// let age = prompt('сколько вам лет?', '18');
+// if (checkAge(age)){
+//     alert("доступ разршен");
+// }
+// else {
+//     alert('доступ запрещен');
+// }
+
+function showPrimes(n) {
+    nextPrime: for (let i = 2; i < n; i++) {
+  
+      for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+      }
+  
+      alert( i ); // простое
+    }
+  }
